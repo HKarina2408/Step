1f22ea41bb3c101546811c4cc46b72ca1093a499
HKarina2408 <gnatyukkarina2408@gmail.com>
2024-02-02 00:02:13 +0200

    commit

---
+++ JS/Basics/lesson-1.js
@@ -0,0 +1,99 @@
+// Data types
+
+// Primitives
+
+// Numbers: 1 2 3 123 (BigInt)
+Number;
+12;
+
+// String: "Hello world" '123'
+String;
+("Hello world");
+
+// Boolean: true/false
+Boolean;
+true;
+false;
+
+// Undefined: undefined
+undefined;
+
+// Null
+null;
+
+// Symbol: Symbol
+// -
+
+// BigInt: 1
+// -
+
+// Object - orientired data types:
+
+// Object: {}, [], function() {}, Map, Set, null.
+
+const a = 10;
+let b = 20;
+// // var c = 10; - not useful
+
+const sum = ((a + b) * 10) / 5;
+
+console.log(sum);
+console.log(100 / 5);
+
+const message = "Hello world!";
+let message2 = "I am programmer";
+
+console.log(message);
+console.log(message2);
+console.log("Karina");
+
+console.log(message + message2);
+// console.log(message - message2); Not works
+
+const isAdmin = true;
+const isUser = false;
+
+let user = undefined;
+console.log(user);
+
+user = "Karina";
+console.log(user);
+
+user = "Karina24";
+console.log(user);
+
+user = null;
+console.log(user);
+
+let counter = 0;
+
+counter = counter + 1;
+counter = counter + 100;
+
+console.log(counter);
+
+let greetingMessage = "";
+greetingMessage = greetingMessage + "H";
+greetingMessage = greetingMessage + "e";
+greetingMessage = greetingMessage + "l";
+greetingMessage = greetingMessage + "l";
+greetingMessage = greetingMessage + "o";
+
+console.log(greetingMessage);
+
+// Object
+
+const obj = {
+  name: "Karina",
+  age: 24,
+  isAdmin: true,
+  info: undefined,
+};
+
+console.log(obj);
+console.log(obj.name);
+
+console.log(window);
+
+window.console.log ("Hello from window");
+
